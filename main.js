44dcc64e4a927d488b7e9fcd6da4335d0fa9d8f4
jefferson0301 <jefferson.s.p.s.p@gmail.com>
2024-01-29 10:35:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+let telefone = ''
+function adicionaInputTelefone(valor){
+    document.querySelector('.inputTelefone').value = telefone
+}
+
+
+function pegarValor1(){
+    let valor = document.querySelector('.btn-1').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor2(){
+    let valor = document.querySelector('.btn-2').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor3(){
+    let valor = document.querySelector('.btn-3').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor4(){
+    let valor = document.querySelector('.btn-4').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor5(){
+    let valor = document.querySelector('.btn-5').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor6(){
+    let valor = document.querySelector('.btn-6').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor7(){
+    let valor = document.querySelector('.btn-7').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor8(){
+    let valor = document.querySelector('.btn-8').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor9(){
+    let valor = document.querySelector('.btn-9').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValor0(){
+    let valor = document.querySelector('.btn-0').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValorMultiplicar(){
+    let valor = document.querySelector('.btn-multiplicar').value
+    adicionaInputTelefone(telefone+=valor)
+}
+
+function pegarValorJogoVelha(){
+    let valor = document.querySelector('.btn-jogo-velha').value
+    adicionaInputTelefone(telefone+=valor)
+}
